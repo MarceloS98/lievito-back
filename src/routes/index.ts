5f540623f2ef900abe40d7933ad0f9411a997faf
Diana Nakagawa <drcnakagawa@gmail.com>
2023-10-18 21:40:16 -0300

    Add the product route to the routerApi

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
-import express, { Application } from "express";
+import express, {Application} from "express";
 
 import ingredientsRouter from "./ingredients.routes";
+import productRouter from "./product.routes";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api/v1", router);
   router.use("/ingredients", ingredientsRouter);
+  router.use("/products", productRouter);
 }
 
 export default routerApi;
