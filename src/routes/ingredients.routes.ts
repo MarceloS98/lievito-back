2a0aadf0abb65d43ac0ff85da7e69ac43683927e
Marcelo Sosa <sosamarcelo92@gmail.com>
2023-10-26 17:53:54 -0300

    finish ingredients routes

--- src/routes/ingredients.routes.ts
+++ src/routes/ingredients.routes.ts
@@ -3,43 +3,91 @@ import {
   createIngredient,
   deleteIngredient,
   getIngredients,
+  getIngredient,
   updateIngredient,
 } from "../services/ingredient.services";
-import validatorHandler from "../middlewares/validationHandler";
 import {
   createIngredientSchema,
   getIngredientSchema,
+  deleteIngredientSchema,
+  updateIngredientSchema,
 } from "../schemas/ingredient.schema";
+import validatorHandler from "../middlewares/validationHandler";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const ingredients = await getIngredients();
-  res.json(ingredients);
+router.get("/", async (req, res, next) => {
+  try {
+    const ingredients = await getIngredients();
+    res.json(ingredients);
+  } catch (error) {
+    next(error);
+  }
 });
+
+router.get(
+  "/:ingredient_id",
+  validatorHandler(getIngredientSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const ingredient = await getIngredient(Number(req.params.ingredient_id));
+
+      if (!ingredient) {
+        return res.status(404).json({
+          message: "Ingredient not found",
+        });
+      }
+
+      res.json(ingredient);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.post(
   "/",
   validatorHandler(createIngredientSchema, "body"),
-  async (req, res) => {
-    const newIngredient = await createIngredient(req.body);
-    res.json(newIngredient);
+  async (req, res, next) => {
+    try {
+      const newIngredient = await createIngredient(req.body);
+      res.json(newIngredient);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
-router.put("/:ingredient_id", async (req, res) => {
-  const updatedIngredient = await updateIngredient(
-    Number(req.params.ingredient_id),
-    req.body
-  );
-  res.json(updatedIngredient);
-});
+router.patch(
+  "/:ingredient_id",
+  validatorHandler(getIngredientSchema, "params"),
+  validatorHandler(updateIngredientSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const updatedIngredient = await updateIngredient(
+        Number(req.params.ingredient_id),
+        req.body
+      );
+      res.json(updatedIngredient);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.delete("/:ingredient_id", async (req, res) => {
-  const deletedIngredient = await deleteIngredient(
-    Number(req.params.ingredient_id)
-  );
-  res.json(deletedIngredient);
-});
+router.delete(
+  "/:ingredient_id",
+  validatorHandler(deleteIngredientSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const deletedIngredient = await deleteIngredient(
+        Number(req.params.ingredient_id)
+      );
+      res.json(deletedIngredient);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
