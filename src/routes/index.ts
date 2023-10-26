866939cefe50de140a689f2aba8a03d93b118b70
Marcelo Sosa <sosamarcelo92@gmail.com>
2023-10-26 17:57:24 -0300

    add new routes to router api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import express, { Application } from "express";
 
 import ingredientsRouter from "./ingredients.routes";
+import ingredientsStockRouter from "./ingredientsStock.routes";
+import outgoingIngredientRouter from "./outgoingIngredient.routes";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api/v1", router);
   router.use("/ingredients", ingredientsRouter);
+  router.use("/ingredientsStock", ingredientsStockRouter);
+  router.use("/outgoingIngredient", outgoingIngredientRouter);
 }
 
 export default routerApi;
